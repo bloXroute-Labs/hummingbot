7f84ede6f149eeecfd8128900607814e23dddcf7
Kaushik Donthi <kaushikdr@gmail.com>
2023-01-09 16:10:49 -0800

    Fixed up the Exchange class

--- hummingbot/connector/exchange/bloxroute_openbook/bloxroute_openbook_exchange.py
+++ hummingbot/connector/exchange/bloxroute_openbook/bloxroute_openbook_exchange.py
@@ -1,51 +1,30 @@
-import asyncio
-import math
 from decimal import Decimal
-from typing import TYPE_CHECKING, Any, Dict, List, Optional, Tuple
+from decimal import Decimal
+from typing import Any, Dict, List, Optional, TYPE_CHECKING, Tuple
 
-from bidict import bidict
-from bxsolana import provider
+from bxsolana.provider import WsProvider
 
 from hummingbot.connector.constants import s_decimal_NaN
 from hummingbot.connector.exchange.bloxroute_openbook import (
     bloxroute_openbook_constants as CONSTANTS,
-    bloxroute_openbook_utils,
     bloxroute_openbook_web_utils as web_utils,
 )
-
+from hummingbot.connector.exchange.bloxroute_openbook.bloxroute_openbook_api_order_book_data_source import \
+    BloxrouteOpenbookAPIOrderBookDataSource
 from hummingbot.connector.exchange.bloxroute_openbook.bloxroute_openbook_auth import BloxrouteOpenbookAuth
-# from hummingbot.connector.exchange.bloxroute_openbook.bloxroute_openbook_utils import (
-#     OrderTypeToBlxrOrderType,
-#     TradeTypeToSide,
-# )
 from hummingbot.connector.exchange_py_base import ExchangePyBase
 from hummingbot.connector.trading_rule import TradingRule
-from hummingbot.connector.utils import combine_to_hb_trading_pair
 from hummingbot.core.data_type.common import OrderType, TradeType
-from hummingbot.core.data_type.in_flight_order import InFlightOrder, OrderState, OrderUpdate, TradeUpdate
+from hummingbot.core.data_type.in_flight_order import InFlightOrder, OrderUpdate, TradeUpdate
 from hummingbot.core.data_type.order_book_tracker_data_source import OrderBookTrackerDataSource
-from hummingbot.core.data_type.trade_fee import AddedToCostTradeFee, TokenAmount, TradeFeeBase
+from hummingbot.core.data_type.trade_fee import AddedToCostTradeFee
 from hummingbot.core.data_type.user_stream_tracker_data_source import UserStreamTrackerDataSource
-from hummingbot.core.event.events import MarketEvent, OrderFilledEvent
-from hummingbot.core.utils.async_utils import safe_gather
 from hummingbot.core.web_assistant.web_assistants_factory import WebAssistantsFactory
 
 if TYPE_CHECKING:
     from hummingbot.client.config.config_helpers import ClientConfigAdapter
 
 s_logger = None
-
-# class TradingPair():
-#     def __init__(self, baseToken: str, quoteToken: str):
-#         self.baseToken = baseToken
-#         self.quoteToken = quoteToken
-#
-#     @classmethod
-#     def fromString(self, trading_pair: str) -> 'TradingPair':
-#         tokens = trading_pair.split("-")
-#         if len(tokens) != 2:
-#             raise Exception("trading pair had more than three tokens")
-#         return TradingPair(baseToken=tokens[0], quoteToken=tokens[1])
 
 class BloxrouteOpenbookExchange(ExchangePyBase):
     """
@@ -60,11 +39,12 @@ class BloxrouteOpenbookExchange(ExchangePyBase):
 
     def __init__(self,
                  client_config_map: "ClientConfigAdapter",
-                 bloxroute_api_key: str,
+                 auth_header: str,
                  solana_wallet_public_key: str,
                  solana_wallet_private_key: str,
-                 # trading_pairs_to_payer_address: Optional[Dict[str, str]] = None,
-                 # trading_required: bool = True,
+                 trading_pairs: Optional[List[str]] = None,
+                 #trading_pairs_to_payer_address: Optional[Dict[str, str]] = None,
+                 #trading_required: bool = True,
                  ):
         """
         :param auth_header: The bloxRoute Labs authorization header to connect with solana trader api
@@ -74,16 +54,13 @@ class BloxrouteOpenbookExchange(ExchangePyBase):
         """
 
         self.logger().exception("creating blox route exchange")
-        self.logger().exception("api key is" + bloxroute_api_key)
+        self.logger().exception("auth header is" + auth_header)
         self.logger().exception("pub key is" + solana_wallet_public_key)
         self.logger().exception("private key is" + solana_wallet_private_key)
 
-        self._auth_header: str = bloxroute_api_key
-        self._sol_wallet_public_key = solana_wallet_public_key
-        self._sol_wallet_private_key = solana_wallet_private_key
-        self._provider = WsProvider(auth_header=auth_header, private_key=secret_key)
+        self._ws_provider = WsProvider(auth_header=auth_header, private_key=solana_wallet_private_key)
+        self._trading_pairs = trading_pairs
         # self._trading_required = trading_required
-        # self._trading_pairs_to_payer_address = trading_pairs_to_payer_address
 
         super().__init__(client_config_map)
         self.real_time_balance_update = False # TODO add functionality for this
