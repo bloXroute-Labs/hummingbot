4d073048d32a85201c5dc6cd786a04870167a028
Kaushik Donthi <kaushikdr@gmail.com>
2023-01-11 19:46:25 -0800

    write tests for orderbook manager

--- test/connector/exchange/bloxroute_openbook/mock_provider.py
+++
@@ -1,5 +0,0 @@
-from bxsolana.provider import Provider
-class MockProvider():
-
-    async def connect(self):
-        pass
--- test/connector/exchange/bloxroute_openbook/test_bloxroute_openbook_orderbook_manager.py
+++ test/connector/exchange/bloxroute_openbook/test_bloxroute_openbook_orderbook_manager.py
@@ -1,31 +1,24 @@
 import asyncio
-import unittest
 from collections.abc import AsyncGenerator
-from unittest.mock import AsyncMock, MagicMock, Mock, patch
+from unittest.mock import AsyncMock, patch
 
 import aiounittest
 import bxsolana.provider.grpc
-import pytest
 from bxsolana_trader_proto import GetOrderbookResponse, GetOrderbooksStreamResponse, OrderbookItem
+from typing import List, Tuple
 
 from hummingbot.connector.exchange.bloxroute_openbook.bloxroute_openbook_orderbook_manager import (
     BloxrouteOpenbookOrderbookManager,
-    Orderbook,
 )
 
-# initalize orderbook
-# apply update
-
-# get orderbook
-# get best price
 
 class TestOrderbookManager(aiounittest.AsyncTestCase):
     @patch('bxsolana.provider.GrpcProvider.get_orderbook')
     async def test_initalize_orderbook(self, mock: AsyncMock):
         provider = bxsolana.provider.GrpcProvider()
 
-        bids = [OrderbookItem(price=5, size=2), OrderbookItem(price=6, size=7)]
-        asks = [OrderbookItem(price=2, size=3), OrderbookItem(price=3, size=4)]
+        bids = orders([(5, 2), (6, 7)])
+        asks = orders([(3, 4), (4, 4)])
         mock.return_value = GetOrderbookResponse(
             market="SOLUSDC",
             market_address="SOL_USDC_Market",
@@ -43,7 +36,7 @@ class TestOrderbookManager(aiounittest.AsyncTestCase):
         self.assertListEqual(asks, ob.asks)
 
         self.assertEqual((5, 2), ob_manager.get_price_with_opportunity_size("SOLUSDC", True))
-        self.assertEqual((3, 4), ob_manager.get_price_with_opportunity_size("SOLUSDC", False))
+        self.assertEqual((4, 4), ob_manager.get_price_with_opportunity_size("SOLUSDC", False))
 
         await ob_manager.stop()
 
@@ -52,8 +45,9 @@ class TestOrderbookManager(aiounittest.AsyncTestCase):
     async def test_apply_update(self, orderbook_mock: AsyncMock, orderbook_stream_mock: AsyncMock):
         provider = bxsolana.provider.GrpcProvider()
 
-        bids = [OrderbookItem(price=5, size=2), OrderbookItem(price=6, size=7)]
-        asks = [OrderbookItem(price=3, size=3), OrderbookItem(price=4, size=4)]
+        # same as first test
+        bids = orders([(5, 2), (6, 7)])
+        asks = orders([(3, 4), (4, 4)])
         orderbook_mock.return_value = GetOrderbookResponse(
             market="SOLUSDC",
             market_address="SOL_USDC_Market",
@@ -61,9 +55,9 @@ class TestOrderbookManager(aiounittest.AsyncTestCase):
             asks=asks,
         )
 
-        new_bids = [OrderbookItem(price=10, size=2), OrderbookItem(price=12, size=7)]
-        new_asks = [OrderbookItem(price=2, size=3), OrderbookItem(price=3, size=4)]
-
+        # new values
+        new_bids = orders([(10, 2), (12, 7)])
+        new_asks = orders([(2, 3), (3, 4)])
         orderbook_stream_mock.return_value = async_generator("SOLUSDC", new_bids, new_asks)
 
         ob_manager = BloxrouteOpenbookOrderbookManager(provider, ["SOLUSDC"])
@@ -79,6 +73,14 @@ class TestOrderbookManager(aiounittest.AsyncTestCase):
         self.assertEqual((3, 4), ob_manager.get_price_with_opportunity_size("SOLUSDC", False))
 
         await ob_manager.stop()
+
+
+def orders(price_and_sizes: List[Tuple[int, int]]) -> List[OrderbookItem]:
+    orderbook_items = []
+    for price, size in price_and_sizes:
+        orderbook_items.append(OrderbookItem(price=price, size=size))
+
+    return orderbook_items
 
 
 async def async_generator(market, bids, asks) -> AsyncGenerator:
