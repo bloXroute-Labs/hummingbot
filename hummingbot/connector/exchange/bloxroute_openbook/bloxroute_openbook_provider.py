15824c8ee9202cd3638207a8afa0984d8fa534fb
Kaushik Donthi <kaushikdr@gmail.com>
2023-02-28 21:21:08 -0800

    Updated bloxroute provider implementation to use `Task` instead of `Event`

--- hummingbot/connector/exchange/bloxroute_openbook/bloxroute_openbook_provider.py
+++ hummingbot/connector/exchange/bloxroute_openbook/bloxroute_openbook_provider.py
@@ -4,18 +4,24 @@ import bxsolana
 
 
 class BloxrouteOpenbookProvider(bxsolana.provider.WsProvider):
-    _provider_connected = asyncio.Event()
+    _provider_connect_task: asyncio.Task
 
     def __init__(self, endpoint: str, auth_header: str, private_key: str):
         super().__init__(endpoint=endpoint, auth_header=auth_header, private_key=private_key)
+        self._connect()
 
-    async def connect(self):
-        await super().connect()
-        self._provider_connected.set()
+    def _connect(self):
+        self._provider_connect_task = asyncio.create_task(super().connect())
 
     async def wait_connect(self):
-        await self._provider_connected.wait()
+        if self._provider_connect_task:
+            await self._provider_connect_task
+        else:
+            raise Exception("provider_connect_task not initialized")
 
     @property
     def connected(self) -> bool:
-        return self._provider_connected.is_set()
+        if self._provider_connect_task:
+            return self._provider_connect_task.done()
+        else:
+            raise Exception("provider_connect_task not initialized")
